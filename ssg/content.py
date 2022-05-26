a7c080881b3d716584025f46818148b159728ba2
Kenneth Cox <differentialcalc1050@gmail.com>
2022-05-26 09:22:01 -0600

    Content

--- ssg/content.py
+++ ssg/content.py
@@ -1,3 +1,8 @@
 import re
 from collections.abc import Mapping
 from yaml import load, FullLoader
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
