de623fb20d9e2952258b7b6e3307a964baa70b0f
Kenneth Cox <differentialcalc1050@gmail.com>
2022-05-25 22:36:02 -0600

    read

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,8 +5,12 @@ from pathlib import Path
 class Parser:
     extensions: List[str] = []
 
-    def valid_extension(self, extension):
+    def valid_extension(self, extension: str):
         return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self, path: Path):
+        with open(path, "r+", "utf-8") as file:
+            return file.read()
