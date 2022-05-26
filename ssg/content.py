8573852ed80daabf290f7af2da1d6dcbc8186b9b
Kenneth Cox <differentialcalc1050@gmail.com>
2022-05-26 09:31:27 -0600

    Update

--- ssg/content.py
+++ ssg/content.py
@@ -6,3 +6,8 @@ from yaml import load, FullLoader
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self, cls, string):
+        _, fm, content = self.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
