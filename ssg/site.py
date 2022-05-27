aa4f9bf1a9baa7e4174536dff5b0f427e38368ee
Kenneth Cox <differentialcalc1050@gmail.com>
2022-05-26 19:59:41 -0600

    update

--- ssg/site.py
+++ ssg/site.py
@@ -30,7 +30,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            Site.error("Not Implemented")
 
     @staticmethod
     def error(message):
