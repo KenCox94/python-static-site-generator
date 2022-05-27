7b93e235ff68fa0d00c4c0178faf1fe195fd268b
Kenneth Cox <differentialcalc1050@gmail.com>
2022-05-26 19:38:00 -0600

    update

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,3 +47,6 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(
                 path.name, content)
         )
+
+class ReStructuredTextParser(Parser):
+    extension = [".rst"]
